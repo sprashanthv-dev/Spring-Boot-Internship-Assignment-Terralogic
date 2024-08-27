41c943250f1ec2985028d1fb514ffc56cdc7f795
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-08-26 22:46:20 -0700

    feat: Project setup + gitignore

---
+++ nodeanalysis/src/main/java/com/sprashanthv/nodeanalysis/NodeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.sprashanthv.nodeanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeAnalysisApplication.class, args);
+	}
+
+}
---
+++ nodeanalysis/src/test/java/com/sprashanthv/nodeanalysis/NodeanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprashanthv.nodeanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
