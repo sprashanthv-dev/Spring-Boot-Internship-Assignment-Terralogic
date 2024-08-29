b557bd231313ed3d9ab964cb6ab0072e3daa842e
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-08-28 17:28:20 -0700

    feat: Added node repository unit tests

--- nodeanalysis/src/test/java/com/sprashanthv/nodeanalysis/repository/NodeRepositoryTests.java
+++ nodeanalysis/src/test/java/com/sprashanthv/nodeanalysis/repository/NodeRepositoryTests.java
@@ -1,7 +1,9 @@
 package com.sprashanthv.nodeanalysis.repository;
 
 import com.sprashanthv.nodeanalysis.config.TestConfig;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,10 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import com.sprashanthv.nodeanalysis.model.Node;
 import org.springframework.context.annotation.Import;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 @DataJdbcTest
 @Import(TestConfig.class)
@@ -21,27 +27,81 @@ public class NodeRepositoryTests {
     @Autowired
     private NodeRepository nodeRepository;
 
+    private Node node1;
+    private Node node2;
+
+    @BeforeEach
+    public void setup() {
+        // Arrange
+        node1 = new Node();
+
+        node1.setNodeId("T1");
+        node1.setName("Node 1");
+        node1.setDescription("Node 1 Description");
+        node1.setMemo("Node 1 Memo");
+        node1.setType("TRANSPONDER");
+        node1.setParentGroupName("Group 1");
+        node1.setParentGroupId("G1");
+        node1.setParentName("Parent 1");
+
+        node2 = new Node();
+
+        node2.setNodeId("T2");
+        node2.setName("Node 2");
+        node2.setDescription("Node 2 Description");
+        node2.setMemo("Node 2 Memo");
+        node2.setType("ROADM");
+        node2.setParentGroupName("Group 2");
+        node2.setParentGroupId("G2");
+        node2.setParentName("Parent 2");
+    }
+
     @Test
     public void NodeRepository_Save_ReturnsSavedNode() {
-        // Arrange
-        Node node = new Node();
-
-        node.setNodeId("T1");
-        node.setName("Node 1");
-        node.setDescription("Node 1 Description");
-        node.setMemo("Node 1 Memo");
-        node.setType("TRANSPONDER");
-        node.setParentGroupName("Group 1");
-        node.setParentGroupId("G1");
-        node.setParentName("Parent 1");
-
         // Act
-        Node savedNode = nodeRepository.save(node);
+        Node savedNode = nodeRepository.save(node1);
 
         // Assert
         Assertions.assertThat(savedNode).isNotNull();
         Assertions.assertThat(savedNode.getNodeId()).isEqualTo("T1");
     }
+
+    @Test
+    public void NodeRepository_FindAll_ReturnsMoreThanOneNode() {
+        nodeRepository.save(node1);
+        nodeRepository.save(node2);
+
+        Iterable<Node> nodeIterable = nodeRepository.findAll();
+        Iterator<Node> nodeIterator = nodeIterable.iterator();
+
+        List<Node> nodes = new ArrayList<>();
+
+        while (nodeIterator.hasNext()) {
+            nodes.add(nodeIterator.next());
+        }
+
+        Assertions.assertThat(nodes).isNotNull();
+        Assertions.assertThat(nodes.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void NodeRepository_SaveAll_ReturnsAllSavedNodes() {
+        // Act
+        List<Node> nodesToBeSaved = new ArrayList<>(List.of(node1, node2));
+        Iterable<Node> savedNodes = nodeRepository.saveAll(nodesToBeSaved);
+
+        Iterator<Node> nodeIterator = savedNodes.iterator();
+
+        List<Node> nodes = new ArrayList<>();
+
+        while (nodeIterator.hasNext()) {
+            nodes.add(nodeIterator.next());
+        }
+
+        Assertions.assertThat(nodes).isNotNull();
+        Assertions.assertThat(nodes.size()).isEqualTo(2);
+    }
+
 
 
 }
